e603aeb00b28714e69dff5a3ddbd173949668bdf
Alexandre Lision <alexandre.lision@gmail.com>
2014-02-20 10:56:45 -0500

    Fix include of google play service

--- src/com/grepsound/GrepSoundService.java
+++ src/com/grepsound/GrepSoundService.java
@@ -4,9 +4,14 @@ import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
 import android.util.Log;
+import com.google.android.gms.auth.GoogleAuthException;
 import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.common.Scopes;
 import com.soundcloud.api.ApiWrapper;
 import com.soundcloud.api.Request;
 import com.soundcloud.api.Token;
@@ -23,33 +28,104 @@ public class GrepSoundService extends Service {
     private static String CLIENT_ID = "398a7f28d61b10d5ee14fcb8bff95d68";
     private static String CLIENT_SECRET = "398a7f28d61b10d5ee14fcb8bff95d68";
     private AccountManager mAccountManager;
+    boolean shouldStop;
+    OAuthThread mThread;
 
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
+    Messenger mMessenger = new Messenger(new IncomingHandler());
 
-    public void registerMe() {
-        ApiWrapper wrapper = new ApiWrapper(CLIENT_ID, CLIENT_SECRET, null, null);
+    /**
+     * Handler of incoming messages from clients.
+     */
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
 
-
-        getAccountNames();
-        /*try {
-            Token tok = wrapper.login("Alexandre Lision", "password", Token.SCOPE_NON_EXPIRING);
-            HttpResponse resp = wrapper.get(Request.to("/me"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
-    }
-
-    private String[] getAccountNames() {
-        mAccountManager = AccountManager.get(this);
-        Account[] accounts = mAccountManager.getAccountsByType(
-                GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE);
-        String[] names = new String[accounts.length];
-        for (int i = 0; i < names.length; i++) {
-            names[i] = accounts[i].name;
-            Log.i(TAG, accounts[i].name);
+            }
         }
-        return names;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(TAG, "onStartCommand");
+        return START_STICKY;
+    }
+
+    @Override
+    public void onCreate() {
+        Log.i(TAG, "onCreate");
+        mThread = new OAuthThread();
+        mThread.start();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i(TAG, "onDestroy");
+
+        mThread.setRunning(false);
+        try {
+            mThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return mMessenger.getBinder();
+    }
+
+    private class OAuthThread extends Thread {
+
+        private boolean isRunning;
+
+        public OAuthThread() {
+            super();
+        }
+
+        public void setRunning(boolean b) {
+            isRunning = b;
+        }
+
+        @Override
+        public void run() {
+            isRunning = true;
+            registerMe();
+        }
+
+        public void registerMe() {
+            ApiWrapper wrapper = new ApiWrapper(CLIENT_ID, CLIENT_SECRET, null, null);
+
+            getAccountNames();
+
+            try {
+                String token = GoogleAuthUtil.getToken(getBaseContext(), "alexandre.lision@gmail.com", Scopes.PLUS_ME);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (GoogleAuthException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                Token tok = wrapper.login("", "", Token.SCOPE_NON_EXPIRING);
+                HttpResponse resp = wrapper.get(Request.to("/me"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private String[] getAccountNames() {
+            mAccountManager = AccountManager.get(GrepSoundService.this);
+            Account[] accounts = mAccountManager.getAccountsByType(GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE);
+
+            String[] names = new String[accounts.length];
+            Log.i(TAG, "getAccountNames:"+names.length);
+            for (int i = 0; i < names.length; i++) {
+                names[i] = accounts[i].name;
+                Log.i(TAG, accounts[i].name);
+            }
+            return names;
+        }
     }
 }
